c0648a6c121a518bfe1d1390a376d86cfe73cc18
zhangxiang958 <958033967@qq.com>
2017-06-21 18:32:39 +0800

    the forEach problem is not fucking done

--- README.md
+++ README.md
@@ -1,15 +1,7 @@
-# ReadmeSample
-
-> 本篇已经收录到 [fetool\['杂七杂八'\]](https://github.com/nieweidong/fetool#other)
-
-#### 项目高大上的第一步就是 ___包装___，所以来看看README的书写套路吧:)
-
-#### 每一项都不是必须，还请自行参考
-
+# Lazyload
 ---
 
-## 示例的项目名称为： ZhuangDeYiShouHaoBi
-
+> 假如有人问劳资，为什么能想到这个伟大的Idea ```ZhuangDeYiShouHaoBi``` ，劳资会告诉丫：“因为装逼”。
 ## Goal and Philosophy
 
   “目标以及哲学”，一个标题瞬间就让项目的气质提升了八个档次。
@@ -20,17 +12,11 @@
 
   康忙昂，北鼻。
 
-## Why ZhuangDeYiShouHaoBi
+## Why lazylad
 
-  自问自答，为毛会有这个项目。
+  This Plug 
+  Because of my project -- wishwall,  
 
-> 假如有人问劳资，为什么能想到这个伟大的Idea ```ZhuangDeYiShouHaoBi``` ，劳资会告诉丫：“因为装逼”。
-
-## Requirements
-
-  描述下必要的前提条件。
-
-  比如：基于koa的项目，就需要 ```node >= 0.11.9```
 
 ## Installation
 
@@ -38,7 +24,12 @@
 
 ## Usage
 
-  北鼻，这个肿么使用呢？
+```
+<script src="static.com/lazyload.js"></script>
+<script>
+  Lazyload();
+</script>
+```
 
 ## How It Works
 
@@ -48,19 +39,7 @@
 
   肿么开始呢？给爷拉出来溜溜。
 
-## More
 
-  相关推荐来几发。最好是贴一些国内外 **较高级的** 、**切实有用的** 的地址，方便大家延伸阅读。
-
-  也更显得偶们专业嘛，最起码证明鸟劳资也是查了很多相关资料的。
-
-## Credits
-
-  介绍项目成员
-
-## Contribute
-
-  采纳的PR贡献者列表，假如真的能有可以加上这项:)
 
 ## License
 
--- index.js
+++ index.js
@@ -14,38 +14,46 @@
         placeholder: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXYzh8+PB/AAffA0nNPuCLAAAAAElFTkSuQmCC'
     }
 
-    function Lazyload(selector, options){
+    var Lazyload = {
+        init: function(selector, options){
 
-        selector = selector || '.lazyload';
-        options = options || {};
-        offset = options.offset || 0;
-        this.updated(selector);
-        loadImage(imgList);
+            selector = selector || '.lazyload';
+            options = options || {};
+            offset = options.offset || 0;
+            this.updated(selector);
+            loadImage(imgList);
+        },
+        updated: function(selector){
+            var elements = doc.querySelectorAll(selector);
+            var placeholder = settings.placeholder;
+
+            [].slice.call(elements).forEach(function(el, i){
+                
+                if(is(el, 'img')) {
+
+                    el.src = placeholder;
+                }
+                imgList.push(el);
+            });
+        }
     };
-
-    Lazyload.prototype.updated = function(selector){
-        
-        var elements = doc.querySelectorAll(selector);
-        var placeholder = settings.placeholder;
-
-        [].slice.call(elements).forEach(function(el, i){
-            el.src = placeholder;
-            imgList.push(el);
-        });
-        console.log(imgList);
-    }
 
     function loadImage(imgList) {
 
-        for(var i = imgList.length - 1; i >= 0; i--) {
-            
-            var el = imgList[i];
-            if(isShow(el)) {
-                el.setAttribute('src', el.getAttribute('data-original'));
+        imgList.forEach(function(el, i){
+             if(isShow(el)) {
+                
+                if(is(el, 'img')) {
+                    
+                    el.setAttribute('src', el.getAttribute('data-original'));
+                } else {
+                    
+                    el.style.backgroundImage = "url('" + el.getAttribute('data-original') + "')"
+                }
+
                 el.className = el.className.replace(/(\s|^)lazyload(\s|$)/, '');
-                imgList.splice(i, 1);
             }
-        }
+        });
     }
 
     function isShow(el) {
@@ -53,6 +61,13 @@
         return (position.top >= 0 && position.left >= 0 && position.top) <= (window.innerHeight || document.documentElement.clientHeight) + parseInt(settings.offset);
     }
     
+    function is(el, type) {
+        if(el.nodeName.toLowerCase() === type) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     function throttle(context, callback, args, delay, duration){
         
@@ -78,10 +93,10 @@
         }
     }
 
-    win.Lazyload = Lazyload;
-
     var self = this;
     window.addEventListener('scroll', function(){
         throttle(loadImage, loadImage, [imgList], 500, 2000)();
     });
+
+    win.Lazyload = Lazyload;
 })(window, document);
